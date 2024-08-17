d16bf71fb898ba516f6da1bf31dbfc13173bebbd
root <anon@ubicoders.com>
2024-08-17 23:46:35 +0000

    updated

---
+++ launch/hexdrone.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='webcam_img_proc',
+            executable='node_webcam',
+            name='node_webcam',
+            output='screen', 
+        ),
+        Node(
+            package='aruco_ps', 
+            executable='body_aruco_node', 
+            name='body_aruco_node',  
+            output='screen',             
+        ),
+        Node(
+            package="ubicoders_hexdrone",
+            executable="hexdrone_ips_pub",
+            name="hexdrone_ips_pub",
+            output="screen",
+        )
+    ])
